89422fa4ee3e40b62fd73dbcf84ec989fee80645
weltonlopes <welton.lps@hotmail.com>
2022-07-13 20:43:14 -0300

    terminei

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,14 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
 import entities.Product;
 
@@ -13,6 +16,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		String path = "c:\\arquivocsv\\products.csv";
 		
 		List<Product> products = new ArrayList<>();
@@ -32,6 +36,28 @@ public class Program {
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
+		
+		path = "c:\\arquivocsv\\";
+		boolean sucess = new File(path + "out").mkdir();
+		System.out.println("Directory created successfully:" + sucess);
+		path = "c:\\arquivocsv\\out\\summary.csv";
+		
+		List<String> newArc = new ArrayList<>();
+		for (Product p : products) {			
+			newArc.add(p.toString());			
+		}
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+			for (String line: newArc) {
+				bw.write(line);
+				bw.newLine();
+			}		
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+				
+		
 		
 		System.out.println("PRODUCTS:");
 		for (Product p: products) {
