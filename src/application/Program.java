064ee0c02fb3ada237ae3d9a5b889a24ce0c65e8
weltonlopes <welton.lps@hotmail.com>
2022-07-17 11:22:25 -0300

    exercicio Lambda stream

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,34 +3,33 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import entities.Employee;
+
+// ESSE AQUI FOI DIFICIL, TIVE QUE FICAR OLHANDO NO RESOLVIDO...
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
 		
-		System.out.print("Enter file full path: ");
-		String path = sc.nextLine();
-		Map<String, Integer> votes = new LinkedHashMap<>();
+		String path = "c:\\projetos\\in.txt";
+		List<Employee> list = new ArrayList<>();
 
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-
+					
 			String line = br.readLine();
 			while (line != null) {
 				String[] fields = line.split(",");
 				String name = fields[0];
-				int count = Integer.parseInt(fields[1]);
-				if (votes.containsKey(name)) {
-					int votesSoFar = votes.get(name);
-					votes.put(name, votesSoFar + count);
-				}
-				else {
-					votes.put(name, count);
-				}
+				String email = fields[1];
+				double salary = Double.parseDouble(fields[2]);
+				list.add(new Employee(name, email, salary));
 				line = br.readLine();
 			}
 
@@ -38,12 +37,19 @@ public class Program {
 			System.out.println(e.getMessage());
 		}
 		
-		for(String key: votes.keySet()) {
-			System.out.println(key + " " + votes.get(key)); 
-		}
+		double salary = 2000.0;
+		List<String> emailList = list.stream()
+				.filter(x -> x.getSalary() > salary)
+				.map(x -> x.getEmail())
+				.collect(Collectors.toList());
+		System.out.println("Emails > 2000.0: ");
+		emailList.forEach(System.out::println);
 		
-		sc.close();
-		
+		double sum = list.stream()
+				.filter(x -> x.getName().charAt(0)=='M') 
+				.map(x -> x.getSalary())
+				.reduce(0.0, (x , y) -> x + y);
+		System.out.printf("Soma do salario comecando com M: %.2f" , sum);
 
 	}
 
